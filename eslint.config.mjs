2e9bc961092a7f78b8eb538f040cb3833a47216e
Thea <142231299+QueenWoofie@users.noreply.github.com>
2025-05-19 16:59:47 +0200

    ESLint update
    Fixed to comply with module 1 -setup +ran prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,27 +1,24 @@
-import js from "@eslint/js";
 import globals from "globals";
-import { defineConfig } from "eslint/config";
+import pluginJs from "@eslint/js";
 
-// This is a configuration file for ESLint, a static code analysis tool for identifying problematic patterns in JavaScript code.
+// This file is used to configure ESLint for the project.
 
-export default defineConfig([
 /** @type {import('eslint').Linter.Config[]} */
-	export default [
-	  {
-	    languageOptions: {
-	      globals: {
-	        ...globals.browser,
-	        describe: true,   // Used for grouping tests
-	        test: true,       // Used to create tests
-	        it: true,        // Alternative way to create tests
-	        expect: true,    // Used for test assertions
-	        require: true,   // Used in Node.js files like Tailwind config
-	        module: true,    // Used in Node.js files like Tailwind config
-	        process: true,   // Used for environment variables later
-	      },
-	    },
-	  },
-	  pluginJs.configs.recommended,
-	];
-])
+export default [
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
 
+        describe: true, // Used for grouping tests
+        test: true, // Used to create tests
+        it: true, // Alternative way to create tests
+        expect: true, // Used for test assertions
+        require: true, // Used in Node.js files like Tailwind config
+        module: true, // Used in Node.js files like Tailwind config
+        process: true, // Used for environment variables later
+      },
+    },
+  },
+  pluginJs.configs.recommended,
+];
