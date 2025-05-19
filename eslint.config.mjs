9e9a46111a575cedd63c68ea2f8f63c417a0e1f8
Thea <142231299+QueenWoofie@users.noreply.github.com>
2025-05-19 16:33:21 +0200

    Update eslint
    Based on original structure provided by the ESLint install

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,14 +2,26 @@ import js from "@eslint/js";
 import globals from "globals";
 import { defineConfig } from "eslint/config";
 
+// This is a configuration file for ESLint, a static code analysis tool for identifying problematic patterns in JavaScript code.
+
 export default defineConfig([
-  {
-    files: ["**/*.{js,mjs,cjs}"],
-    plugins: { js },
-    extends: ["js/recommended"],
-  },
-  {
-    files: ["**/*.{js,mjs,cjs}"],
-    languageOptions: { globals: globals.browser },
-  },
-]);
+/** @type {import('eslint').Linter.Config[]} */
+	export default [
+	  {
+	    languageOptions: {
+	      globals: {
+	        ...globals.browser,
+	        describe: true,   // Used for grouping tests
+	        test: true,       // Used to create tests
+	        it: true,        // Alternative way to create tests
+	        expect: true,    // Used for test assertions
+	        require: true,   // Used in Node.js files like Tailwind config
+	        module: true,    // Used in Node.js files like Tailwind config
+	        process: true,   // Used for environment variables later
+	      },
+	    },
+	  },
+	  pluginJs.configs.recommended,
+	];
+])
+
