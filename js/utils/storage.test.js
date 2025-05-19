8c9fe474017fec2f20f348156aee205aa790281e
Thea <142231299+QueenWoofie@users.noreply.github.com>
2025-05-19 19:01:45 +0200

    Added vitest & test function for isActivePath + getUserName (WIP) - commit attempt 5

---
+++ js/utils/storage.test.js
@@ -0,0 +1,10 @@
+import { describe, it } from "vitest";
+/* import { getUserName } from "./storage.js"; */
+
+// Tests the getUserName function, which retrieves the username from local storage.
+
+describe("getUserName", () => {
+  it("test that it returns the name from the user object in storage", () => {});
+
+  it("test that it returns null when no user exists in storage", () => {});
+});
